08817f2aff56e2ca88ec4869faf2d62e103c64bb
Gocha <gochazautashvili2017@gmail.com>
2024-02-08 19:49:53 +0400

    about

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/header/Header'
-import About from './pages/about'
+import About from './pages/About'
 import Flowers from './pages/Flowers'
 import Profile from './pages/Profile'
 import Register from './pages/auth/Register'
